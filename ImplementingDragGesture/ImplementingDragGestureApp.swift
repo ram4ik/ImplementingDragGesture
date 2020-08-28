fb8024cae1cc1d90f7cbaa440b11bf98b14542b3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-28 17:02:38 +0300

    Initial Commit

---
+++ ImplementingDragGesture/ImplementingDragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementingDragGestureApp.swift
+//  ImplementingDragGesture
+//
+//  Created by ramil on 28.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct ImplementingDragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

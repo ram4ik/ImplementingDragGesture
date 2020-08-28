0921559fba3040769321d422b3cd347cd4790378
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-28 19:42:29 +0300

    detect height

--- ImplementingDragGesture/ContentView.swift
+++ ImplementingDragGesture/ContentView.swift
@@ -14,29 +14,34 @@ struct ContentView: View {
     @GestureState var dragAmount = CGSize.zero
     
     var body: some View {
-        Circle()
-            .foregroundColor(.green)
-            .frame(width: 200, height: 200)
-            .animation(.spring())
-            //.offset(y: self.draggedOffset.height)
-            .offset(dragAmount)
-            
-            .gesture(DragGesture()
-                        .updating($dragAmount) { value, state, transaction in
-                                    state = value.translation
-                                print(value)
-                                print(state)
-                                print(transaction)
-                            
-                                }
-//                        .onChanged({ value in
-//                            draggedOffset = value.translation
-//                            print(value)
-//                        })
-//                        .onEnded({ value in
-//                            draggedOffset = CGSize.zero
-//                        })
-            )
+        VStack {
+            Text("\(score)")
+            Spacer()
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 200, height: 200)
+                .animation(.spring())
+                .offset(y: self.draggedOffset.height)
+                //.offset(dragAmount)
+                
+                .gesture(DragGesture()
+    //                        .updating($dragAmount) { value, state, transaction in
+    //                                    state = value.translation
+    //                                print(value)
+    //                                print(state)
+    //                                print(transaction)
+    //
+    //                                }
+                            .onChanged({ value in
+                                draggedOffset = value.translation
+                                score += Int(-value.translation.height / 10)
+                            })
+                            .onEnded({ value in
+                                draggedOffset = CGSize.zero
+                            })
+                )
+            Spacer()
+        }
     }
 }
 

2f2c8018007d36e12d0912dd40f616e8e9552e9f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-28 18:46:42 +0300

    @GestureState

--- ImplementingDragGesture/ContentView.swift
+++ ImplementingDragGesture/ContentView.swift
@@ -11,20 +11,31 @@ struct ContentView: View {
     @State private var score: Int = 0
     @State private var draggedOffset = CGSize.zero
     
+    @GestureState var dragAmount = CGSize.zero
+    
     var body: some View {
         Circle()
             .foregroundColor(.green)
             .frame(width: 200, height: 200)
             .animation(.spring())
-            .offset(y: self.draggedOffset.height)
+            //.offset(y: self.draggedOffset.height)
+            .offset(dragAmount)
+            
             .gesture(DragGesture()
-                        .onChanged({ value in
-                            draggedOffset = value.translation
-                            print(draggedOffset)
-                        })
-                        .onEnded({ value in
-                            draggedOffset = CGSize.zero
-                        })
+                        .updating($dragAmount) { value, state, transaction in
+                                    state = value.translation
+                                print(value)
+                                print(state)
+                                print(transaction)
+                            
+                                }
+//                        .onChanged({ value in
+//                            draggedOffset = value.translation
+//                            print(value)
+//                        })
+//                        .onEnded({ value in
+//                            draggedOffset = CGSize.zero
+//                        })
             )
     }
 }
